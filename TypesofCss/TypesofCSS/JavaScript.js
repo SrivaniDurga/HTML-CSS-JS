e80b990573c59a1067a037dc4d740af33ce4682f
Srivani Durga <srivanisiri01@gmail.com>
2023-12-27 14:20:01 +0530

    Add project files.

---
+++ TypesofCss/TypesofCSS/JavaScript.js
@@ -0,0 +1,9 @@
+﻿function displayGreeting(displayName) {
+    console.log("Hello");
+    displayName();
+    console.log("Good Morning!");
+}
+function displayRahul() {
+    console.log("Rahul");
+}
+displayGreeting(displayRahul);
